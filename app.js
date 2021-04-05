f9286305d9cb96ba726cbe7efd3647f7b2637162
Md. Shah Alam Talukder <73011157+stratul@users.noreply.github.com>
2021-04-05 15:15:46 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,114 @@
+let signInBtn = document.getElementById('sign-in');
+let signUpBtn = document.getElementById('sign-up');
+let signIn = document.getElementById('sign-in-form');
+let signUp = document.getElementById('sign-up-form');
+let closeBtn1 = document.getElementById('close1');
+let closeBtn2 = document.getElementById('close2');
+
+
+let imgBtn = document.getElementsByTagName('img');
+let fullImages = document.getElementById('full-images');
+let fullImg = document.getElementById('image');
+let closeBtn3 = document.getElementById('close3');
+
+signInBtn.addEventListener('click', function () {
+    signIn.style.display = 'block';
+});
+closeBtn1.addEventListener('click', function () {
+    signIn.style.display = 'none';
+});
+signUpBtn.addEventListener('click', function () {
+    signUp.style.display = 'block';
+});
+closeBtn2.addEventListener('click', function () {
+    signUp.style.display = 'none';
+});
+
+
+imgBtn[0].addEventListener('click', function() {
+    fullImages.style.display = 'block';
+    fullImg.src = this.src;
+});
+imgBtn[1].addEventListener('click', function() {
+    fullImages.style.display = 'block';
+    fullImg.src = this.src;
+});
+imgBtn[2].addEventListener('click', function() {
+    fullImages.style.display = 'block';
+    fullImg.src = this.src;
+});
+imgBtn[3].addEventListener('click', function() {
+    fullImages.style.display = 'block';
+    fullImg.src = this.src;
+});
+imgBtn[4].addEventListener('click', function() {
+    fullImages.style.display = 'block';
+    fullImg.src = this.src;
+});
+imgBtn[5].addEventListener('click', function() {
+    fullImages.style.display = 'block';
+    fullImg.src = this.src;
+});
+imgBtn[6].addEventListener('click', function() {
+    fullImages.style.display = 'block';
+    fullImg.src = this.src;
+});
+imgBtn[7].addEventListener('click', function() {
+    fullImages.style.display = 'block';
+    fullImg.src = this.src;
+});
+imgBtn[8].addEventListener('click', function() {
+    fullImages.style.display = 'block';
+    fullImg.src = this.src;
+});
+imgBtn[9].addEventListener('click', function() {
+    fullImages.style.display = 'block';
+    fullImg.src = this.src;
+});
+imgBtn[10].addEventListener('click', function() {
+    fullImages.style.display = 'block';
+    fullImg.src = this.src;
+});
+imgBtn[11].addEventListener('click', function() {
+    fullImages.style.display = 'block';
+    fullImg.src = this.src;
+});
+imgBtn[12].addEventListener('click', function() {
+    fullImages.style.display = 'block';
+    fullImg.src = this.src;
+});
+imgBtn[13].addEventListener('click', function() {
+    fullImages.style.display = 'block';
+    fullImg.src = this.src;
+});
+imgBtn[14].addEventListener('click', function() {
+    fullImages.style.display = 'block';
+    fullImg.src = this.src;
+});
+imgBtn[15].addEventListener('click', function() {
+    fullImages.style.display = 'block';
+    fullImg.src = this.src;
+});
+imgBtn[16].addEventListener('click', function() {
+    fullImages.style.display = 'block';
+    fullImg.src = this.src;
+});
+imgBtn[17].addEventListener('click', function() {
+    fullImages.style.display = 'block';
+    fullImg.src = this.src;
+});
+imgBtn[18].addEventListener('click', function() {
+    fullImages.style.display = 'block';
+    fullImg.src = this.src;
+});
+imgBtn[19].addEventListener('click', function() {
+    fullImages.style.display = 'block';
+    fullImg.src = this.src;
+});
+imgBtn[20].addEventListener('click', function() {
+    fullImages.style.display = 'block';
+    fullImg.src = this.src;
+});
+closeBtn3.addEventListener('click', function () {
+    fullImages.style.display = 'none';
+});
